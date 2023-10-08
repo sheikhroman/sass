01556fcb5fb66c2194408a8de0c26904797e05ea
Roman <sheikhroman042@gmail.com>
2023-10-08 12:59:31 +0600

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+export default function HomePage() {
+    return (
+        <div className="grid-halves h-screen-navbar">
+            <div className="bg-teal border-right">
+                <div className="column-padding">
+                    <div className="tablet-centered">
+                        <div className="content-grid">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
