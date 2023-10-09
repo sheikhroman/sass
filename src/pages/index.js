d61b55bb26a7f6ac2109a65f32bc18d537ce5f42
Roman <sheikhroman042@gmail.com>
2023-10-09 15:33:58 +0600

    font page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import hero from '../../public/assets/hero.png'
+
 export default function HomePage() {
     return (
         <div className="grid-halves h-screen-navbar">
@@ -5,8 +9,19 @@ export default function HomePage() {
                 <div className="column-padding">
                     <div className="tablet-centered">
                         <div className="content-grid">
-
+                            <h1>The most <br /> epic products</h1>
+                            <p className="section-subtitle">All the most epic things on the internet, all in one place.</p>
+                            <Link href='/products' className="large-button">
+                                <div className="large-button-text">Explore Products</div>
+                            </Link>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-salmon">
+                <div className="column-padding-centered">
+                    <div className="callout-wrap">
+                        <Image src ={hero} className="callout-image"></Image>
                     </div>
                 </div>
             </div>
