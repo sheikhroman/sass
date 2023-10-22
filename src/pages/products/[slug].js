8b39f057dd7b1758e768ce9730a6f180e2189471
Roman <sheikhroman042@gmail.com>
2023-10-22 23:43:33 +0600

    slug done

--- src/pages/products/[slug].js
+++ src/pages/products/[slug].js
@@ -1,23 +1,53 @@
+import Image from "next/image"
+import PromoCard from "src/products/components/PromoCard"
 import { supabase } from "supabase"
 
-export default function ProductPage() {
-    return <div>Products Page</div>
+export default function ProductPage({ product }) { 
+    console.log(product)
+    return (
+        <section className="product-section">
+            <article className="product">
+                <div className="product-wrap">
+                    <Image width={1000} height={300} src={`/assets/${product.slug}.png`}/>
+                </div>
+                <section>
+                    <header>
+                        <h3>{product.name}</h3>
+                    </header>
+                    <section>
+                        <div className="product">
+                            <p>{product.description}</p>
+                        </div>
+                    </section>
+                </section>
+                <section><PromoCard /></section>
+            </article>
+        </section>
+    )
 }
 export async function getStaticPaths() {
-    const { data: products } = await supabase.from('product').select('slug')
-    const getStaticPaths = products.map(product=> ({
+    const { data: products } = await supabase.from("product").select("slug")
+    const paths = products.map(products => ({
         params: {
-            slug: product.slug
+            slug: products.slug
         }
     }))
+
     return {
-        paths: [{ params: { slug: "1" } }, { params: { id: "2" } }],
+        paths,
         fallback: false,
     }
 }
 export async function getStaticProps(context) {
     const slug = context.params.slug
+    const { data: product } = await supabase
+        .from("product")
+        .select("*")
+        .eq("slug", slug)
+        .single()
     return {
-        props: {post:{}},
+        props: {
+            product,
+        },
     }
 }
--- src/products/components/PromoCard.js
+++ src/products/components/PromoCard.js
@@ -1,1 +1,15 @@
-export default function PromoCard() {}
+import Link from "next/link";
+
+export default function PromoCard() {
+    return (
+    <section>
+            <div className="">
+                <div className="bg-salmon">
+                    <h4>Get Instant Access</h4>
+                    <p style={{fontSize: '1.2rem'}}>Access this products plus dozens of others when you subcribe.</p>
+                </div>
+            </div>     
+            <Link href="/pricing" className="primary-button">Purchase</Link>
+    </section>
+    )
+}
